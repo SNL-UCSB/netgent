b2fef3462db38c9dc828131970725eb2f32c6262
jaber-the-great <yjaber@ucsb.edu>
2025-10-16 17:04:06 +0000

    deleting old code

--- src/actions/__init__.py
+++
@@ -1,5 +0,0 @@
-from .browser_manager import BrowserManager
-from .controller import HumanInputController
-from .state_executor import StateExecutor
-
-__all__ = ["BrowserManager", "Controller", "StateExecutor"]
--- src/dom/__init__.py
+++
@@ -1,4 +0,0 @@
-from .webmarker import mark_dom, unmark_dom
-from .dom_parser import parse_dom
-
-__all__ = ["mark_dom", "unmark_dom", "parse_dom"]
